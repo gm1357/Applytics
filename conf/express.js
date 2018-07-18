d8edd092cd81f4ffc82b9c4096064e22e17cb77b
Gabriel Machado <g_machado1@hotmail.com>
2018-07-18 17:29:44 -0300

    Criado páginas de erro

--- conf/express.js
+++ conf/express.js
@@ -4,6 +4,7 @@ bodyParser = require('body-parser');
 methodOverride = require('method-override')
 expressValidator = require('express-validator');
 favicon = require('serve-favicon');
+require('dotenv').load();
 
 
 module.exports = function() {
@@ -20,8 +21,20 @@ module.exports = function() {
     app.use(favicon('app/public/favicon.ico'));
 
     load('routes', {cwd: 'app'})
-        .then('infra')
-        .into(app);
+    .then('infra')
+    .into(app);
+
+    app.use(function(req, res, next) {
+        res.status(404).render('erros/404');
+    });
+
+    app.use(function(error, req, res, next) {
+        if (process.env.NODE_ENV !== 'development') {
+            res.status(500).render('erros/500');
+            return;
+        }
+        next(error);
+    });
 
     return app;
 }
